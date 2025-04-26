18e6d830beb7e453b8079d02a54f4ac8df868a30
wilson <zhangqiwei0509@gmail.com>
2025-04-26 21:46:19 +0800

    dd

--- git_bt/server/src/migrations/add_featured_fields.sql
+++
@@ -1,4 +0,0 @@
--- 向question_sets表添加isFeatured和featuredCategory字段
-ALTER TABLE question_sets 
-ADD COLUMN is_featured BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN featured_category VARCHAR(50) NULL; 
